7d89448cba9be4ecfe558b2f54695772a99c7944
ziftikhr <ziftikhr@gmail.com>
2025-03-30 09:52:58 +0600

    added backed model for task and now can route to profile of the user which will have the task

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
 const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const profileRoutes = require("./routes/profileRoutes");
 
 app.use(express.json());
 app.use(cors());
@@ -16,7 +18,13 @@ mongoose.connect(mongoUrl, err => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/profile", profileRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
